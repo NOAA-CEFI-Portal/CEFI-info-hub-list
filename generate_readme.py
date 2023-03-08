478203898d330501e9d45a5753e0134e27a37418
C.-W. HSU <chia-wei.hsu@noaa.gov>
2023-03-08 09:53:22 -0700

    linting and add encoding

--- generate_readme.py
+++ generate_readme.py
@@ -1,44 +1,65 @@
+"""
+The script is to generate the CEFI resource list README.md
+
+The resource list is mirroring the https://psl.noaa.gov/data/fisheries/
+search tool which provide a quick way to view the entire list and provide 
+a alternative platform for contributing the resource to be included in the 
+search tool.
+
+"""
 import json
 from mdutils.mdutils import MdUtils
 
+if __name__ == '__main__':
 
-# read json data
-f = open('./data/cefi_list.json')
-data = json.load(f)
+    # read json data (soft link to the original source file)
+    f = open('./data/cefi_list.json', encoding="utf-8")
+    data = json.load(f)
 
+    # create markdown file
+    mdFile = MdUtils(file_name='README')
 
-# create markdown file
-mdFile = MdUtils(file_name='README')
+    # start markdown structure
+    mdFile.new_header(level=1, title='CEFI related resource list')
 
-mdFile.new_header(level=1, title='CEFI related resource list')
+    mdFile.new_paragraph(
+        "This is a curated list for the 'CEFI related resource'"+
+        "on the information hub created for the NOAA Climate, Ecosystems, and Fisheries Initiative ("+
+        mdFile.new_inline_link(
+            link='https://www.fisheries.noaa.gov/topic/climate-change/climate-ecosystems-and-fisheries',
+            text='CEFI'
+            )+
+        "). The goal of the information hub is \n"
+        )
 
-mdFile.new_paragraph("This is a curated list for the 'CEFI related resource'"+
-                     "on the information hub created for the NOAA Climate, Ecosystems, and Fisheries Initiative ("+
-                     mdFile.new_inline_link(link='https://www.fisheries.noaa.gov/topic/climate-change/climate-ecosystems-and-fisheries', text='CEFI')+
-                     "). The goal of the information hub is \n")
-
-mdFile.new_line('> Build a comprehensive Climate, Ecosystems, and Fisheries Initiative Information Hub to provide easy access'+
-                ' to regional ocean model outputs (high spatial resolution reanalysis, hindcasts, predictions, and projections'+
-                ' optimized for management applications), ecosystem projections, and other information relevant to'+
-                ' climate-informed resource management. \n')
-
-
-mdFile.new_line('The complete list here is mirroring the '+
-                mdFile.new_inline_link(link='https://psl.noaa.gov/data/fisheries/', text='CEFI related resource search tool')+
-                '. Through the search tool, users can apply filter and search text to narrow down the list based on the related topics. \n')
-
-mdFile.new_line('We welcome external contribution to this list. Please read the `CONTRIBUTION.md` before submitting suggestion. Thank you! \n')
-
-
-# --- list start
-mdFile.new_header(level=2, title='Mirroring list')
-
-for list in data['lists']:
-    mdFile.new_line(mdFile.new_inline_link(link=list['url'], text=list['title'])+' \n')
-    mdFile.new_line('> '+list['desc'] +'\n')
+    mdFile.new_line(
+        '> Build a comprehensive Climate, Ecosystems, and Fisheries Initiative Information Hub to provide easy access'+
+        ' to regional ocean model outputs (high spatial resolution reanalysis, hindcasts, predictions, and projections'+
+        ' optimized for management applications), ecosystem projections, and other information relevant to'+
+        ' climate-informed resource management. \n'
+        )
 
 
-mdFile.new_table_of_contents(table_title='Contents', depth=2)
+    mdFile.new_line(
+        'The complete list here is mirroring the '+
+        mdFile.new_inline_link(
+            link='https://psl.noaa.gov/data/fisheries/',
+            text='CEFI related resource search tool'
+            )+
+        '. Through the search tool, users can apply filter and search text to narrow down the list based on the related topics. \n')
+
+    mdFile.new_line('We welcome external contribution to this list. Please read the `CONTRIBUTION.md` before submitting suggestion. Thank you! \n')
 
 
-mdFile.create_md_file()
+    # include the list
+    mdFile.new_header(level=2, title='Mirroring list')
+
+    for cefi_list in data['lists']:
+        mdFile.new_line(mdFile.new_inline_link(link=cefi_list['url'], text=cefi_list['title'])+' \n')
+        mdFile.new_line('> '+cefi_list['desc'] +'\n')
+
+    # include the table of content at the top of the file
+    mdFile.new_table_of_contents(table_title='Contents', depth=2)
+
+    # finalize the markdown file and output
+    mdFile.create_md_file()
