8648254df59a6ca13d3751188ffb70c7d141cb77
Mathew Biddle <8480023+MathewBiddle@users.noreply.github.com>
2023-03-28 16:15:27 -0400

    looping over all categories

--- create_issue_template.py
+++ create_issue_template.py
@@ -18,16 +18,28 @@ with open(issue_temp) as f:
 
 
 # Add in dropdown for ctype
-issue_data['body'].append(
-    {'type':'dropdown',
-     'id': 'ctype',
-     'attributes': {'label': cefi_data['categories_definition']['ctype']['name'],
-                    'multiple': 'True',
-                    'options': list(cefi_data['categories_definition']['ctype'].values())[1:]
-                    }
-     }
-)
+# issue_data['body'].append(
+#     {'type':'dropdown',
+#      'id': 'ctype',
+#      'attributes': {'label': cefi_data['categories_definition']['ctype']['name'],
+#                     'multiple': 'True',
+#                     'options': list(cefi_data['categories_definition']['ctype'].values())[1:]
+#                     }
+#      }
+# )
 
-out_fname = 'test.yml'
+# loop over all categories
+for cat in cefi_data['categories_definition'].keys():
+    issue_data['body'].append(
+        {'type': 'dropdown',
+         'id': cat,
+         'attributes': {'label': cefi_data['categories_definition'][cat]['name'],
+                        'multiple': 'True',
+                        'options': list(cefi_data['categories_definition'][cat].values())[1:]
+                        }
+         }
+    )
+
+out_fname = '.github/ISSUE_TEMPLATE/test.yml'
 with open(out_fname, 'w') as f:
     data = yaml.dump(issue_data, f, sort_keys=False, default_flow_style=False)
