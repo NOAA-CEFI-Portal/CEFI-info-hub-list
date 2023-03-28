836989675e498d3bf51d6fb08b0e30499b910d54
Mathew Biddle <8480023+MathewBiddle@users.noreply.github.com>
2023-03-28 16:09:50 -0400

    testing python script to create issue template

---
+++ create_issue_template.py
@@ -0,0 +1,33 @@
+# import json
+import yaml
+import generate_readme
+
+cefi_data = generate_readme.get_cefi_list()
+
+# for key in cefi_data['categories_definition'].keys():
+#     list(cefi_data['categories_definition'][key].values())
+#
+#
+# print(yaml.dump(cefi_data['categories_definition'],default_flow_style=False))
+
+
+issue_temp = ".github/ISSUE_TEMPLATE/add_resource.yml"
+
+with open(issue_temp) as f:
+    issue_data = yaml.load(f,Loader=yaml.loader.SafeLoader)
+
+
+# Add in dropdown for ctype
+issue_data['body'].append(
+    {'type':'dropdown',
+     'id': 'ctype',
+     'attributes': {'label': cefi_data['categories_definition']['ctype']['name'],
+                    'multiple': 'True',
+                    'options': list(cefi_data['categories_definition']['ctype'].values())[1:]
+                    }
+     }
+)
+
+out_fname = 'test.yml'
+with open(out_fname, 'w') as f:
+    data = yaml.dump(issue_data, f, sort_keys=False, default_flow_style=False)
