57d3865a9d7110625d7f8725b0bdfc8bd562c813
Mathew Biddle <8480023+MathewBiddle@users.noreply.github.com>
2023-03-28 16:24:21 -0400

    remove cvar

--- create_issue_template.py
+++ create_issue_template.py
@@ -30,15 +30,16 @@ with open(issue_temp) as f:
 
 # loop over all categories
 for cat in cefi_data['categories_definition'].keys():
-    issue_data['body'].append(
-        {'type': 'dropdown',
-         'id': cat,
-         'attributes': {'label': cefi_data['categories_definition'][cat]['name'],
-                        'multiple': True,
-                        'options': list(cefi_data['categories_definition'][cat].values())[1:]
-                        }
-         }
-    )
+    if cat != 'cvar':
+        issue_data['body'].append(
+            {'type': 'dropdown',
+            'id': cat,
+             'attributes': {'label': cefi_data['categories_definition'][cat]['name'],
+                            'multiple': True,
+                            'options': list(cefi_data['categories_definition'][cat].values())[1:]
+                            }
+            }
+        )
 
 out_fname = '.github/ISSUE_TEMPLATE/test.yml'
 with open(out_fname, 'w') as f:
