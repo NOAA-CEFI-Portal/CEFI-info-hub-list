1e139bcc1b27538a93f9268f12bdc4d243e322f5
C.-W. HSU <11950284+chiaweh2@users.noreply.github.com>
2023-03-30 14:02:32 -0600

    adding GH action for json check

---
+++ check_json.py
@@ -0,0 +1,11 @@
+"""
+The script is to check the json resource file can be read in correctly
+
+Conda env:
+- cefilist 
+
+"""
+from generate_readme import get_cefi_list
+
+data = get_cefi_list()
+print('json file loaded')
