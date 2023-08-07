09e59121c883d5bfb3092e33e2389b85e2c2bccf
C.-W. HSU <11950284+chiaweh2@users.noreply.github.com>
2023-08-07 13:42:25 -0600

    turn off debug

--- generate_pr_from_issue.py
+++ generate_pr_from_issue.py
@@ -63,7 +63,7 @@ if __name__ == '__main__' :
     # cefi list repo location
     ORGNAME = "NOAA-PSL"
     REPO_NAME = "CEFI-info-hub-list"
-    DEBUG = True
+    DEBUG = False
 
     # A token is automatically provided by GitHub Actions
     # ACCESS_TOKEN = "${{ secrets.GITHUB_TOKEN }}"
